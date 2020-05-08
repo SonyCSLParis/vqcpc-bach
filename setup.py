9dedb552dc03e4cb34b9cc3d1989dd379906adc2
Theis Bazin <theis.bazin@outlook.com>
2020-05-08 16:05:53 +0200

    :children_crossing: Add minimal setup.py file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='vqcpc-bach',
+      version='0.0.1',
+      url='https://github.com/SonyCSLParis/vqcpc-bach',
+      author=['Gaëtan Hadjeres', 'Léopold Crestel'],
+      author_email='gaetan.hadjeres@sony.com',
+      license='',
+      packages=['VQCPCB'],
+      zip_safe=False)
